6bf484adab3818e75bceabe4b44e92c7b8073441
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2025-09-01 17:02:35 +0700

    i delete teh simmilar in route

--- node/src/routes/book/about/simiarBookRoute.js
+++ node/src/routes/book/about/simiarBookRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getSimilarBooks } = require("../../../controller/book/about/similarBook");
 // GET /api/books/similar?category=History
-router.get("/similar/:category", getSimilarBooks);
+router.get("/:category", getSimilarBooks);
 
 
 module.exports = router;
