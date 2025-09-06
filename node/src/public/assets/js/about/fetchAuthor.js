7e62b052d433ca7da051a7e290a6e1115520b1ca
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2025-09-06 15:38:06 +0700

    100% work

--- node/src/public/assets/js/about/fetchAuthor.js
+++ node/src/public/assets/js/about/fetchAuthor.js
@@ -8,7 +8,9 @@ fetch(`https://thebooksourcings.onrender.com/api/author/${wikiId}`)
     console.error("fetchAuthor", err.message);
 });
 
-function renderAuthor(data){
+function renderAuthor(response){
+    const data = response.data; // extract the nested data
+
     document.querySelector(".authorFullName").textContent = data.name;
     document.querySelector(".shortDescription").textContent = data.description;
     document.querySelector(".website").href = data.website;
@@ -18,17 +20,15 @@ function renderAuthor(data){
     document.querySelector(".youtube").href = data.youtube;
     document.querySelector(".twitter").href = data.x;
     document.querySelector(".aboutAFN").textContent = data.name;
-    document.querySelector(".Givenname").textContent = data.givenName;
-    document.querySelector(".Familyname").textContent = data.familyName;
+    document.querySelector(".Givenname").textContent = data.givenName.join(", ");
+    document.querySelector(".Familyname").textContent = data.familyName.join(", ");
     document.querySelector(".Dateofbirth").textContent = data.dateOfBirth;
-    document.querySelector(".Placeofbirth").textContent = data.placeOfBirth;
-    document.querySelector(".Dateofdeath").textContent = data.dateOfDeath
-    document.querySelector(".Placeofdeath").textContent = data.placeOfDeath;
-    document.querySelector(".Gender").textContent = data.gender;
-    document.querySelector(".Citizenship").textContent = data.citizenship;
-    document.querySelector(".Occupation").textContent = data.profession;
+    document.querySelector(".Placeofbirth").textContent = data.placeOfBirth.join(", ");
+    document.querySelector(".Dateofdeath").textContent = data.dateOfDeath;
+    document.querySelector(".Placeofdeath").textContent = data.placeOfDeath.join(", ");
+    document.querySelector(".Gender").textContent = data.gender.join(", ");
+    document.querySelector(".Citizenship").textContent = data.citizenship.join(", ");
+    document.querySelector(".Occupation").textContent = data.profession.join(", ");
     document.querySelector(".Summary").textContent = data.summary;
-    document.querySelector(".awards").textContent = data.awards;
-
-
+    document.querySelector(".awards").textContent = data.awards.join(", ");
 }
