51515aa777356eda682ac6d59987967e79c3c958
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2025-09-22 16:59:52 +0700

    update auth

--- node/src/middleware/verifySocketToken.js
+++ node/src/middleware/verifySocketToken.js
@@ -1,19 +1,38 @@
+// const jwt = require("jsonwebtoken");
+
+// const verifySocketToken = (socket, next) => {
+//   const token = socket.handshake.auth?.token || socket.handshake.headers?.authorization?.split(" ")[1];
+
+//   if (!token) {
+//     return next(new Error("Authentication error: Token required"));
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+//     socket.user = decoded;
+//     next();
+//   } catch (err) {
+//     next(new Error("Authentication error: Invalid or expired token"));
+//   }
+// };
+
+// module.exports = verifySocketToken;
+
 const jwt = require("jsonwebtoken");
 
-const verifySocketToken = (socket, next) => {
-  const token = socket.handshake.auth?.token || socket.handshake.headers?.authorization?.split(" ")[1];
-
-  if (!token) {
-    return next(new Error("Authentication error: Token required"));
-  }
-
+function verifySocketToken(socket, next) {
   try {
+    const token = socket.handshake.auth?.token;
+    if (!token) {
+      return next(new Error("Token required"));
+    }
+
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-    socket.user = decoded; // store user_id, memberQid, email, timezone
+    socket.user = decoded; // attach decoded user to socket
     next();
   } catch (err) {
-    next(new Error("Authentication error: Invalid or expired token"));
+    next(new Error("Invalid or expired token"));
   }
-};
+}
 
-module.exports = verifySocketToken;
+module.exports =  verifySocketToken ;
