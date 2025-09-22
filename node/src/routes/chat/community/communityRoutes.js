212df760b4c8f5ad317eac5cd1c2646934aafc92
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2025-09-22 22:09:44 +0700

    no middleware for display

--- node/src/routes/chat/community/communityRoutes.js
+++ node/src/routes/chat/community/communityRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getAllMessages, sendMessage, editMessage, deleteMessage } = require("../../../controller/chat/community/communityController");
 const  verifyHttpToken = require("../../../middleware/verifyHttpToken"); // JWT middleware
 
-router.get("/display", verifyHttpToken, getAllMessages);
+router.get("/display", getAllMessages);
 router.post("/send", verifyHttpToken, sendMessage);
 router.put("/edit", verifyHttpToken, editMessage);
 router.delete("/delete", verifyHttpToken, deleteMessage);
