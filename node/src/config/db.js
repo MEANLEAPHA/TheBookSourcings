0dc744e74c724cd130fa5bbb6462359f259a755d
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2025-12-14 16:30:40 +0700

    gg

--- node/src/config/db.js
+++ node/src/config/db.js
@@ -4,18 +4,21 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 // Create a connection pool
-const dbconfig = mysql.createPool(
-    {
-        host:"srv1656.hstgr.io", //localhost
-        user:"u859618886_otthorAdmin", //root
-        password:"OtthorAnd2005", //password
-        database:"u859618886_otthorDB", //database
-        waitForConnections: true, //wait for connections
-        connectionLimit: 10, //max connections
-        port: 3306, //port
-        queueLimit: 0,//max waiting connections
-    }
-)
+const dbconfig = mysql.createPool({
+  host: "srv1656.hstgr.io",
+  user: "u859618886_otthorAdmin",
+  password: "OtthorAnd2005",
+  database: "u859618886_otthorDB",
+  port: 3306,
+
+  waitForConnections: true,
+  connectionLimit: 2,
+  queueLimit: 0,
+
+  connectTimeout: 30000,      // ⭐ IMPORTANT (30s)
+  enableKeepAlive: true,      // ⭐ prevents idle drop
+  keepAliveInitialDelay: 0    // ⭐ immediate keepalive
+});
 
 // Export the database
 module.exports = dbconfig; 
