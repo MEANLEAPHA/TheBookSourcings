dfcc09350654bfe9b545f460fa59f34fba5d96cf
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2025-12-28 21:30:20 +0700

    idk if it work

--- node/src/public/assets/js/index/index.js
+++ node/src/public/assets/js/index/index.js
@@ -10,7 +10,8 @@ if (!feedSeed) {
 let cursor = 0;
 let isLoading = false;
 // Example: when page first loads
-window.addEventListener('load', () => {
+window.addEventListener("pageshow", (event) => {
+  // fires even when restored from cache
   const newSeed = Math.floor(Math.random() * 1_000_000);
   sessionStorage.setItem("feed_seed", newSeed);
   feedSeed = newSeed;
@@ -21,6 +22,18 @@ window.addEventListener('load', () => {
 
   fetchNextBatch();
 });
+
+// window.addEventListener('load', () => {
+//   const newSeed = Math.floor(Math.random() * 1_000_000);
+//   sessionStorage.setItem("feed_seed", newSeed);
+//   feedSeed = newSeed;
+
+//   cursor = 0;
+//   isLoading = false;
+//   container.innerHTML = "";
+
+//   fetchNextBatch();
+// });
 
 async function fetchNextBatch() {
   if (isLoading) return;
