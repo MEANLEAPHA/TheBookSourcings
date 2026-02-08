4f2f3ec165624aad6b8104762c85dc33b729b224
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-02-08 16:31:00 +0700

    no auth at the route

--- node/src/routes/book/trending/trendingRoutes.js
+++ node/src/routes/book/trending/trendingRoutes.js
@@ -1,12 +1,12 @@
 // Import dependencies using require
 const express = require('express');
 const { getAllTrending, getFeed } = require('../../../controller/book/trending/trendingController');
-
+const {authMiddleware} = require('../../../middleware/authMiddleware');
 const router = express.Router();
 
 // GET /api/trending
-router.get('/trending', getAllTrending);
-router.get('/feed', getFeed);
+router.get('/trending', authMiddleware, getAllTrending);
+router.get('/feed',getFeed);
 
 // Export the router using CommonJS
 module.exports = router;
